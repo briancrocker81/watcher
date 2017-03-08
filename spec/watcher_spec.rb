5d4e48729e40ec85778e705b2997aadb77e5a104
Brian Crocker <brian.crocker@vualto.com>
2017-03-08 22:39:23 +0000

    tidied the rpsec ready to write tests

--- spec/watcher_spec.rb
+++ spec/watcher_spec.rb
@@ -1,4 +1,11 @@
 require_relative 'spec_helper'
 
+include Watcher
+
 describe Watcher do
+  context 'watch directory exists' do
+    it 'should return true when exists' do
+      
+    end
+  end
 end
