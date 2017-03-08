c3cd7d1fcd9f492f50f020beebb34db3bf10f5d9
Brian Crocker <brian.crocker@vualto.com>
2017-03-08 14:50:43 +0000

    copied file to new folder

--- filewatcher.rb
+++ filewatcher.rb
@@ -7,10 +7,28 @@ require_relative 'logger'
 # File.open("logs/watch_log.log", "a") {|f| f.write("----- Log started at " + Time.now.strftime("%d/%m/%Y %H:%M") + " -----" + "\n") }
 
 
+module FolderWatcher
 
-dir = Dir.pwd
+  def watch_folder
+    dir = 'src_ism'
+    FileWatcher.new(dir +'/*').watch do |filename|
+      MyLog.log.info("file: #{filename} has changed")
+      copyfile(filename)
+    end
+  end
 
-FileWatcher.new(dir +'/*').watch do |filename|
-  MyLog.log.info("file: #{filename} has changed")
-  # copyfile(filename)
+  def copyfile(filename)
+    unless filename.include? "_aes"
+      FileUtils.cp filename, "#{filename}_aes.ism"
+      MyLog.log.info("file: #{filename} has copied and renamed to: #{filename}_aes.ism")
+    end
+
+  # if copyfile exists do nothing
+  # else
+  end
+
+  # puts Watcher.instance_methods
 end
+
+include FolderWatcher
+watch_folder
